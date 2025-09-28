847dfd48b98e01444223398f357aee58afe343f2
Martina Massana Massip <martina.massana@estudiantat.upc.edu>
2025-09-28 18:08:15 +0200

    reorganized repo

--- deepfake_recognition/config.py
+++ deepfake_recognition/config.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from loguru import logger
 
 # Load environment variables from .env file if it exists
-load_dotenv()
+# load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
@@ -12,9 +12,8 @@ logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
-INTERIM_DATA_DIR = DATA_DIR / "interim"
+METADATA_DIR = DATA_DIR / "metadata"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
 MODELS_DIR = PROJ_ROOT / "models"
 
