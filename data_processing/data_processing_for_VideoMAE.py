5af11d89e9d95a70cd1961d6d73b381318233b70
maiteblasi <113500169+maiteblasi@users.noreply.github.com>
2025-10-07 14:30:42 +0200

    data_processing_for_VideoMAE.py in english

--- data_processing/data_processing_for_VideoMAE.py
+++ data_processing/data_processing_for_VideoMAE.py
@@ -12,7 +12,7 @@ def video_to_clips(video_path, out_dir, clip_len=16, resize=(224,224)):
             break
         frame = cv2.resize(frame, resize)
         frames.append(frame)
-        # Cuando juntamos clip_len frames → guardamos
+        # Save when joining clip_len frames
         if len(frames) == clip_len:
             clip_name = os.path.join(
                 out_dir,
@@ -29,7 +29,7 @@ def process_folder(in_dir, out_dir, clip_len=16):
         if fname.endswith(".mp4"):
             video_to_clips(os.path.join(in_dir, fname), out_dir, clip_len)
 
-# Procesar dataset completo
+# Process complete dataset
 splits = ["train", "val", "test"] 
 labels = ["real", "fake"]
 
